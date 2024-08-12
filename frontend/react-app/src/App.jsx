c35b27a90e9d694b819748ba78ee579115a86cf9
Rohan Awhad <rohanavad007@gmail.com>
2024-08-12 14:44:26 -0400

    added api key submission page

--- frontend/react-app/src/App.jsx
+++ frontend/react-app/src/App.jsx
@@ -1,22 +1,24 @@
 import './App.css'
 import CitationCard from '@/components/CitationCard'
 import GraphTab from './components/GraphTab'
-import CodeTab from './components/CodeTab'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { useEffect, useRef, useState } from 'react'
 
-import { getChatResponse, getCode, getRefIds, getRefData, getMindmapMd } from '@/api';
+import { getChatResponse, getCode, getRefIds, getRefData, getMindmapMd, validateApiKey } from '@/api';
 import ChatHistory from '@/components/ChatHistory';
 import Suggestions from '@/components/Suggestions';
 
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { Label } from "@/components/ui/label"
 
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { solarizedlight } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 function App() {
+  const [apiKey, setApiKey] = useState(undefined)
+
   const [url, setUrl] = useState(undefined)
   const [citations, setCitations] = useState([])
 
@@ -124,61 +126,92 @@ function App() {
     setGeneratingMsg(true);
   }
 
+  const apiKeyRef = useRef()
+  const errorMsgRef = useRef()
+  const handleApiKeySubmission = (e) => {
+    e.preventDefault();
+    const apiKey = apiKeyRef.current.value;
+    validateApiKey(apiKey).then((isValid) => {
+      if (isValid) {
+        setApiKey(apiKey);
+      } else {
+        errorMsgRef.current.classList.remove('invisible')
+      }
+    });
+  };
 
-  return (
-    <>
-      <Tabs defaultValue="citations" className="w-full h-screen">
-        <TabsList className="grid w-full grid-cols-4 h-fit">
-          <TabsTrigger value="citations" className="text-base font-medium">References</TabsTrigger>
-          <TabsTrigger value="chat" className="text-base font-medium">Chat</TabsTrigger>
-          <TabsTrigger value="graph" className="text-base font-medium">Graph</TabsTrigger>
-          <TabsTrigger value="code" className="text-base font-medium">Code</TabsTrigger>
-        </TabsList>
-        <TabsContent value="citations">
-          <div>
-            {citations && citations.map(data => (
-              <CitationCard
-                key={data.id}
-                id={data.id}
-                first_author={data.first_author_name}
-                title={data.title}
-                why={data.why}
-                contribution={data.contribution}
-                related_works={data.related_works}
-              />
-            ))}
-          </div>
-        </TabsContent>
-        <TabsContent value="chat">
-          <div className="h-screen grid justify-items-end">
-            <ScrollArea className="w-full h-full flex-col" ref={scrollRef}>
-              {messages && <ChatHistory messages={messages} />}
-              {suggestions && <Suggestions suggestions={suggestions} setUserCurrMsg={setUserCurrMsg} />}
-            </ScrollArea>
-            <div className="flex rounded-full border-2 items-center w-full px-3 my-3 h-fit">
-              <Input className='text-base border-none py-2' type="text" placeholder="Message your Paper" onKeyDown={handleKeyDown} onChange={handleInputChange} value={userCurrMsg} />
-              <Button className="text-base my-1 rounded-full w-fit" onClick={handleSendMessage}>
-                Send
-              </Button>
+  let ret = <></>
+  if (apiKey === undefined) {
+    // TODO: (rohan) handle api key request
+    ret = (
+      <div className="grid grid-cols-1 w-full max-w-lg items-center mx-auto">
+        <p className='invisible text-xs mb-2 text-rose-600' ref={errorMsgRef}>Unable to verify API Key</p>
+        <Label className='mb-2' htmlFor="api_key">API Key</Label>
+        <div className='flex flex-row gap-2 w-xl mb-3'>
+          <Input type="password" id="api_key" placeholder="API Key" ref={apiKeyRef} />
+          <Button className='rounded-full' onClick={handleApiKeySubmission}>Submit</Button>
+        </div>
+        <p className="text-gray-500 text-sm mt-2">Please enter your API key to proceed. If you don't have one, kindly visit <a href="https://example.org" className="text-blue-500 hover:text-blue-700">https://example.org</a> to obtain one.</p>
+      </div>
+    )
+  } else {
+    ret = (
+      <>
+        <Tabs defaultValue="citations" className="w-full h-screen">
+          <TabsList className="grid w-full grid-cols-4 h-fit">
+            <TabsTrigger value="citations" className="text-base font-medium">References</TabsTrigger>
+            <TabsTrigger value="chat" className="text-base font-medium">Chat</TabsTrigger>
+            <TabsTrigger value="graph" className="text-base font-medium">Graph</TabsTrigger>
+            <TabsTrigger value="code" className="text-base font-medium">Code</TabsTrigger>
+          </TabsList>
+          <TabsContent value="citations">
+            <div>
+              {citations && citations.map(data => (
+                <CitationCard
+                  key={data.id}
+                  id={data.id}
+                  first_author={data.first_author_name}
+                  title={data.title}
+                  why={data.why}
+                  contribution={data.contribution}
+                  related_works={data.related_works}
+                />
+              ))}
             </div>
-          </div>
-        </TabsContent>
-        <TabsContent value="graph">
-          <GraphTab markdown={markdown} />
-        </TabsContent>
-        <TabsContent value="code">
-          {/*<CodeTab mindmap={markdown} url={url} />*/}
-          {code ? (
-            <SyntaxHighlighter language="python" style={solarizedlight}>
-              {code}
-            </SyntaxHighlighter>
-          ) : (
-            <div>Generating Code</div>
-          )}
-        </TabsContent>
-      </Tabs>
-    </>
-  )
+          </TabsContent>
+          <TabsContent value="chat">
+            <div className="h-screen grid justify-items-end">
+              <ScrollArea className="w-full h-full flex-col" ref={scrollRef}>
+                {messages && <ChatHistory messages={messages} />}
+                {suggestions && <Suggestions suggestions={suggestions} setUserCurrMsg={setUserCurrMsg} />}
+              </ScrollArea>
+              <div className="flex rounded-full border-2 items-center w-full px-3 my-3 h-fit">
+                <Input className='text-base border-none py-2' type="text" placeholder="Message your Paper" onKeyDown={handleKeyDown} onChange={handleInputChange} value={userCurrMsg} />
+                <Button className="text-base my-1 rounded-full w-fit" onClick={handleSendMessage}>
+                  Send
+                </Button>
+              </div>
+            </div>
+          </TabsContent>
+          <TabsContent value="graph">
+            <GraphTab markdown={markdown} />
+          </TabsContent>
+          <TabsContent value="code">
+            {/*<CodeTab mindmap={markdown} url={url} />*/}
+            {code ? (
+              <SyntaxHighlighter language="python" style={solarizedlight}>
+                {code}
+              </SyntaxHighlighter>
+            ) : (
+              <div>Generating Code</div>
+            )}
+          </TabsContent>
+        </Tabs>
+      </>
+    )
+  }
+
+  return ret
 }
 
 export default App
